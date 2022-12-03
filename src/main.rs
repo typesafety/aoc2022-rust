3c21ad9dcf073ce34d79f65233b10e6b5badab9e
typesafety <ailoodee@gmail.com>
2022-12-03 14:57:32 +0100

    Set up main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+mod solutions;
+
+use solutions::day01;
+
+use aoc_table::table_gen::TableGen;
+
+
 fn main() {
-    println!("Hello, world!");
+    // Run solvers and output to a nice table.
+    TableGen::new("AoC 2022 solutions")
+        .add(day01::part1, day01::part2)
+        .run();
 }
---
+++ src/solutions/mod.rs
@@ -0,0 +1,3 @@
+/// Re-export solution modules.
+
+pub mod day01;
