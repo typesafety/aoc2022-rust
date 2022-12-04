63ac730f66812d78f6e44ecda1e6322db4ac7d77
typesafety <ailoodee@gmail.com>
2022-12-04 16:05:37 +0100

    Day 4 part 2

--- src/solutions/day04.rs
+++ src/solutions/day04.rs
@@ -1,3 +1,5 @@
+use workrange::WorkRange;
+
 pub fn part1() -> i32 {
     const INPUT: &str = include_str!("../../inputs/4.input");
     let pairs = parse_input(INPUT).unwrap_or_default();
@@ -10,28 +12,65 @@ pub fn part1() -> i32 {
 }
 
 pub fn part2() -> i32 {
-    0
+    const INPUT: &str = include_str!("../../inputs/4.input");
+    let pairs = parse_input(INPUT).unwrap_or_default();
+
+    pairs
+        .iter()
+        .map(|(r1, r2)| r1.overlaps(r2))
+        .map(i32::from)
+        .sum()
 }
 
-struct Range {
-    start: i32,
-    end: i32,
-}
+mod workrange {
+    use std::ops::RangeInclusive;
 
-impl Range {
-    fn subsumes(&self, victim: &Range) -> bool {
-        self.start <= victim.start && self.end >= victim.end
+    pub struct WorkRange {
+        _range: RangeInclusive<i32>,
+    }
+
+    impl WorkRange {
+        pub fn new(start: i32, end: i32) -> Self {
+            Self {
+                _range: RangeInclusive::new(start, end),
+            }
+        }
+
+        pub fn start(&self) -> &i32 {
+            self._range.start()
+        }
+
+        pub fn end(&self) -> &i32 {
+            self._range.end()
+        }
+
+        pub fn subsumes(&self, victim: &WorkRange) -> bool {
+            self.start() <= victim.start() && self.end() >= victim.end()
+        }
+
+        pub fn overlaps(&self, victim: &WorkRange) -> bool {
+            victim._range.contains(self.start())
+                || victim._range.contains(self.end())
+                || self._range.contains(victim.start())
+                || self._range.contains(victim.end())
+        }
+    }
+
+    impl std::fmt::Display for WorkRange {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "{}-{}", self.start(), self.end())
+        }
     }
 }
 
-fn parse_input(input: &str) -> Result<Vec<(Range, Range)>, ParseError> {
+fn parse_input(input: &str) -> Result<Vec<(WorkRange, WorkRange)>, ParseError> {
     input
         .split('\n')
         .map(parse_row)
-        .collect::<Result<Vec<(Range, Range)>, ParseError>>()
+        .collect::<Result<Vec<(WorkRange, WorkRange)>, ParseError>>()
 }
 
-fn parse_row(input: &str) -> Result<(Range, Range), ParseError> {
+fn parse_row(input: &str) -> Result<(WorkRange, WorkRange), ParseError> {
     match &input.split(',').collect::<Vec<&str>>()[..] {
         [range1, range2] => {
             let range1 = parse_range(range1)?;
@@ -45,12 +84,12 @@ fn parse_row(input: &str) -> Result<(Range, Range), ParseError> {
     }
 }
 
-fn parse_range(input: &str) -> Result<Range, ParseError> {
+fn parse_range(input: &str) -> Result<WorkRange, ParseError> {
     match &input.split('-').collect::<Vec<&str>>()[..] {
         [start, end] => {
             let start = start.parse::<i32>()?;
             let end = end.parse::<i32>()?;
-            Ok(Range { start, end })
+            Ok(WorkRange::new(start, end))
         }
         x => {
             let err_msg = format!("Failed to parse a range from input: {x:?}");
